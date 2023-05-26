693373d239abc11d4989b6a53311714642fcf0a1
github_federicolss <thiagoVernanchet.2022@gmail.com>
2023-05-26 17:59:06 -0300

    cambios

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import React from 'react'
  import App from './App';
  import { BrowserRouter } from 'react-router-dom';
  import { Provider } from 'react-redux';
- import store from './redux/store'
+ import store from './redux/store '
  const root = ReactDOM.createRoot(document.getElementById('root'));
  
  root.render(
